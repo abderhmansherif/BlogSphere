ba10cf28aa8fab66cacfe7d6e2ef2f3848274ef2
Abderhman <bebosherif202@gmail.com>
2026-01-24 23:45:19 +0200

    Refactored Login page

---
+++ BlogSphere.WebUI/Components/Shared/AppInputBase.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BlogSphere.WebUI.Components.Shared
+{
+    public class AppInputBase : InputBase<string>
+    {
+        protected override bool TryParseValueFromString(string? value, [MaybeNullWhen(false)] out string result, [NotNullWhen(false)] out string? validationErrorMessage)
+        {
+
+
+
+            if (string.IsNullOrEmpty(value))
+            {
+                result = string.Empty;
+                validationErrorMessage = "Input string is null or empty";
+                return false;
+            }
+
+            result = value;
+            validationErrorMessage = null;
+            return true;
+        }
+    }
+}
